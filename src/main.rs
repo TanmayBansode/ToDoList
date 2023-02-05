d2a42bfe5f5a6efd9f62542c4a662c0b94e14281
Parth Jadhav <jadhavparth99@gmail.com>
2023-02-05 12:17:09 +0530

    feat: refactor

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,94 @@
 use std::io;
 
+fn add_new_task(tasks: &mut Vec<String>) {
+    println!("-------------------------------------------------");
+    println!(" ");
+    println!(">>Enter Tasks<<");
+    println!(" ");
+    println!("Type and Enter to add a Task.");
+    println!("(Type done when finished)");
+    println!(" ");
+    loop {
+        let mut input = String::new();
+
+        io::stdin().read_line(&mut input).expect("Error");
+        input.pop();
+
+        match input == "done" {
+            true => {
+                println!("-------------------------------------------------");
+                println!(" ");
+                println!(">>TASKS<<");
+                println!(" ");
+                let mut i: u32 = 1;
+                for el in tasks {
+                    println!("{i}.{}", el);
+                    i += 1;
+                }
+
+                break;
+            }
+
+            _ => {
+                tasks.push(input);
+            }
+        }
+    }
+    println!(" ");
+    println!("-------------------------------------------------");
+}
+
+fn print_tasks(tasks: &Vec<String>) {
+    for (i, el) in tasks.iter().enumerate() {
+        println!("{}. {}", i + 1, el);
+    }
+}
+
+fn show_tasks(tasks: &Vec<String>) {
+    println!("-------------------------------------------------");
+    println!(" ");
+    println!(">>TASKS<<");
+    println!(" ");
+    print_tasks(&tasks);
+    println!(" ");
+    println!("-------------------------------------------------");
+}
+
+fn mark_as_done(tasks: &mut Vec<String>) {
+    loop {
+        println!("-------------------------------------------------");
+        println!(" ");
+        println!(">>TASKS<<");
+        println!(" ");
+        print_tasks(&tasks);
+        println!(" ");
+        println!(
+            " (Press 0 to go back to menu) \n\
+    Serial Number of task that is done :"
+        );
+        let mut index = String::new();
+        io::stdin()
+            .read_line(&mut index)
+            .expect("Failed to read line");
+
+        let index: usize = match index.trim().parse() {
+            Ok(num) => num,
+            Err(_) => 0,
+        };
+
+        match index == 0 {
+            true => {
+                println!(" ");
+                println!("-------------------------------------------------");
+                break;
+            }
+            _ => {
+                tasks.remove(index - 1);
+            }
+        }
+    }
+}
+
 fn main() {
     let mut tasks = Vec::new();
 
@@ -9,9 +98,9 @@ fn main() {
         println!(" ");
         println!(
             "1 > Add tasks\n\
-    2 > View List\n\
-    3 > Mark Tasks as DONE
-     "
+             2 > View List\n\
+             3 > Mark Tasks as DONE
+            "
         );
         println!("(Choose Action)");
 
@@ -28,102 +117,20 @@ fn main() {
 
         match action {
             1 => {
-                println!("-------------------------------------------------");
-                println!(" ");
-                println!(">>Enter Tasks<<");
-                println!(" ");
-                println!("Type and Enter to add a Task.");
-                println!("(Type done when finished)");
-                println!(" ");
-                loop {
-                    let mut input = String::new();
-
-                    io::stdin().read_line(&mut input).expect("Error");
-                    input.pop();
-
-                    match input == "done" {
-                        true => {
-                            println!("-------------------------------------------------");
-                            println!(" ");
-                            println!(">>TASKS<<");
-                            println!(" ");
-                            let mut i: u32 = 1;
-                            for el in &tasks {
-                                println!("{i}.{}", el);
-                                i += 1;
-                            }
-
-                            break;
-                        }
-
-                        _ => {
-                            tasks.push(input);
-                        }
-                    }
-                }
-                println!(" ");
-                println!("-------------------------------------------------");
+                add_new_task(&mut tasks);
+                // only use `continue` if there is a chance that below code also has possibility to run. If user enters 1, then there is no point in running below code.
                 continue;
             }
 
             2 => {
-                println!("-------------------------------------------------");
-                println!(" ");
-                println!(">>Tasks TO DO<<");
-                println!(" ");
-                let mut i: u32 = 1;
-                for el in &tasks {
-                    println!("{i}. {}", el);
-                    i += 1;
-                }
-                println!(" ");
-                println!("Press any key to go back");
-
-                let mut i = String::new();
-                io::stdin().read_line(&mut i).expect("Failed to read line");
-
-                println!(" ");
-                println!("-------------------------------------------------");
+                show_tasks(&tasks);
                 continue;
             }
 
-            3 => loop {
-                println!("-------------------------------------------------");
-                println!(" ");
-                println!(">>TASKS<<");
-                println!(" ");
-                let mut i: u32 = 1;
-                for el in &tasks {
-                    println!("{i}.{}", el);
-                    i += 1;
-                }
-
-                println!(" ");
-                println!(
-                    " (Press 0 to go back to menu) \n\
-            Serial Number of task that is done :"
-                );
-                let mut index = String::new();
-                io::stdin()
-                    .read_line(&mut index)
-                    .expect("Failed to read line");
-
-                let index: usize = match index.trim().parse() {
-                    Ok(num) => num,
-                    Err(_) => 0,
-                };
-
-                match index == 0 {
-                    true => {
-                        println!(" ");
-                        println!("-------------------------------------------------");
-                        break;
-                    }
-                    _ => {
-                        tasks.remove(index - 1);
-                    }
-                }
-            },
+            3 => {
+                mark_as_done(&mut tasks);
+                continue;
+            }
 
             _ => {
                 println!("-------------------------------------------------");
